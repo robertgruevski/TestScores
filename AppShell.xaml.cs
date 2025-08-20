f7f013d44e5e336ea3f385c0887252487326c704
robertgruevski <robertgruevski@outlook.com>
2025-08-20 17:47:37 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestScores
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestScores")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestScores.Pages")]
